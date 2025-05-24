bcaff948fe364fcdd8f824ab269eeaa3425aa652
dietken1 <dietken1@ajou.ac.kr>
2025-05-24 15:05:23 +0900

    feat: 프로젝트 시작

---
+++ backend/A-Rate/src/main/java/com/example/arate/ARateApplication.java
@@ -0,0 +1,13 @@
+package com.example.arate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ARateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ARateApplication.class, args);
+    }
+
+}
---
+++ backend/A-Rate/src/test/java/com/example/arate/ARateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ARateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
